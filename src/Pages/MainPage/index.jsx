7a70fac89c132309a5d90873228d849a21ddcdcc
예빈 <yebin060310@gmail.com>
2023-07-26 13:34:31 +0900

    Add charkra

--- src/Pages/MainPage/index.jsx
+++ src/Pages/MainPage/index.jsx
@@ -1,5 +1,22 @@
 import * as S from "./style";
+import { Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
 const Main = () => {
-  return <S.Container>하이 ㅋㅋ</S.Container>;
+  return (
+    <S.Main>
+      <S.Container>
+        <Card>
+          <CardHeader />
+          <CardBody>
+            <S.Title>
+              아재개그 세상에 <br />
+              오신것을 환영합니다!
+            </S.Title>
+          </CardBody>
+          <CardFooter />
+        </Card>
+        <img src="https://item.kakaocdn.net/do/35d21721120852d73f512b9bc17f36689f5287469802eca457586a25a096fd31" />
+      </S.Container>
+    </S.Main>
+  );
 };
 export default Main;
--- src/Pages/MainPage/style.js
+++ src/Pages/MainPage/style.js
@@ -1,5 +1,34 @@
 import styled from "styled-components";
-
+export const Main = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
 export const Container = styled.div`
-  width: 100px;
+  background-color: #81bef7;
+  width: 600px;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+export const TitleContainer = styled.div`
+  width: 500px;
+  height: 200px;
+  background-color: white;
+  border-radius: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+export const Title = styled.h1`
+  font-family: "HSSaemaul-Regular";
+  font-size: 60px;
+  color: white;
+  text-shadow: 10px 10px 10px #f2f2f2;
+  -webkit-text-stroke-width: 0.1px;
+  -webkit-text-stroke-color: gray;
+  text-align: center;
 `;
