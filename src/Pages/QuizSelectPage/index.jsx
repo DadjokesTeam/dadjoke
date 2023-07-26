4af6278c68cf2e52d6586f1873c3ee47c76ebf8d
SeoJumee <official.jumee@gmail.com>
2023-07-26 22:41:22 +0900

    Add quiz select page

---
+++ src/Pages/QuizSelectPage/index.jsx
@@ -0,0 +1,29 @@
+import * as S from './style';
+import * as C from '../../components';
+import { Link } from 'react-router-dom';
+const Quiz = () => {
+  const arr = [
+    { id: 1, content: 'level 1' },
+    { id: 2, content: 'level 2' },
+    { id: 3, content: 'level 3' },
+  ];
+
+  return (
+    <S.Main>
+      <S.Container>
+        <S.Title>단계를 선택해주세요</S.Title>
+        <S.ButtonContainer>
+          {arr.map(({ id, content }) => {
+            return (
+              <Link to={`level/${id}`}>
+                <C.Button content={content} />
+              </Link>
+            );
+          })}
+        </S.ButtonContainer>
+      </S.Container>
+    </S.Main>
+  );
+};
+
+export default Quiz;
