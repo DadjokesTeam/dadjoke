3437b681645054e34e1bd951e29cb5c829acfb39
윤수용 <yunsuyong739@naver.com>
2023-07-27 10:49:28 +0900

    add 양자택일button

--- src/Pages/SelectPage/index.jsx
+++ src/Pages/SelectPage/index.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 import * as S from "./style";
 
 const Select = () => {
+
   return (
     <S.Container>
       <S.SelectTitle>원하시는 게임을 선택해주세요!</S.SelectTitle>
-      <img src="https://noiseandkisses.com/apps/note/wp-content/uploads/2023/03/menu_cinamon.png" />
       <S.ButtonContainer>
         <Link to="/quiz">
           <Button content="아재개그 퀴즈 하기" />
@@ -15,6 +15,10 @@ const Select = () => {
         <S.SelectMessage>or</S.SelectMessage>
         <Link to="/picture">
           <Button content="틀린그림찾기 하기" />
+        </Link>
+        <S.SelectMessage>or</S.SelectMessage>
+        <Link to="/">
+          <Button content="양자택일 게임" />
         </Link>
       </S.ButtonContainer>
     </S.Container>
